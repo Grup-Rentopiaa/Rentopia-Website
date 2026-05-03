77c3772cfe89036a3ade8b27f58717580bb57466
RiskaAml <amelriskaaa@gmail.com>
2026-05-03 05:51:38 -0700

    landing page sampai auth semua berhasil

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 5faaabe411eb544031c5b32dc821a949287d8b24
 );
