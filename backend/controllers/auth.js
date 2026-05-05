f418265239ac0548d1e77ab34ff0b73c724b89f7
RiskaAml <amelriskaaa@gmail.com>
2026-05-05 04:30:55 -0700

    fix(auth): add prisma import in verifyOtp controller and fix SMTP port for Docker

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,4 @@
+const prisma = require('../db')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {findByEmail, createUser, createAuth, findAuthByUserId, saveOtp, findAuthByOtp, updatePassword, findById} = require('../models/user')
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer')
 
 const sendEmail = async (to, otp) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
@@ -15,4 +17,5 @@ const sendEmail = async (to, otp) => {
         text: `Kode OTP kamu adalah: ${otp}`
     })
 }
+
 module.exports = sendEmail
